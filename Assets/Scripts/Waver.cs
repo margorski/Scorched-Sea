52dc825d0c25bee50445cf1c2ca318ba0610acdc
Siara Siarzewski <sephichi@gmail.com>
2017-01-21 08:46:55 +0100

    Fala ma GetY

---
+++ Assets/Scripts/Waver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waver : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float GetY(float x)
+    {
+        return Mathf.Sin(Mathf.Deg2Rad * x);
+    }
+}
