18a94964e652e990b31b69c63d911c0142de1f59
Siara Siarzewski <sephichi@gmail.com>
2017-01-21 14:45:46 +0100

    Main scene

--- Assets/Scripts/Waver.cs
+++ Assets/Scripts/Waver.cs
@@ -9,6 +9,7 @@ public class Waver : MonoBehaviour {
     public float Pindol200 = 45f;    //for 1 -> 1 sec is 1 degree
     public float DegreesPhase = 0f;
     public float Frequency = 10f; //1 in position is 10 in angle
+    public float StandingWaveCoeff = 10f;
 
     private Wave _mainWave;
     private readonly List<Wave> _addWaves = new List<Wave>();
@@ -49,10 +50,11 @@ public class Waver : MonoBehaviour {
     public void Init()
     {
         _currentTurn = GameManager.Instance.TurnCounter;
-        _mainWave = new Wave(MainAmplitude, Pindol200, DegreesPhase, Frequency);
-        _addWaves.Add(new Wave(MainAmplitude * 0.2f, Pindol200, DegreesPhase, Frequency * 20f, -1, 0.5f, 6.2f));
-        _addWaves.Add(new Wave(MainAmplitude * 0.2f, Pindol200, DegreesPhase, Frequency * 10f, 5, 0.5f, 6.1f));
-        _addWaves.Add(new Wave(MainAmplitude * 0.2f, Pindol200, DegreesPhase, Frequency * 10f, 5, 0.5f, -3f));
+        _mainWave = new Wave(MainAmplitude, Pindol200, DegreesPhase, Frequency, StandingWaveCoeff);
+        // to play with stuff
+        //_addWaves.Add(new Wave(MainAmplitude * 0.2f, Pindol200, DegreesPhase, Frequency * 20f, 1f, -1, 0.5f, 6.2f));
+        //_addWaves.Add(new Wave(MainAmplitude * 0.2f, Pindol200, DegreesPhase, Frequency * 10f, 1f, 5, 0.5f, 6.1f));
+        //_addWaves.Add(new Wave(MainAmplitude * 0.2f, Pindol200, DegreesPhase, Frequency * 10f, 1f, 5, 0.5f, -3f));
     }
 
     /// <summary>
@@ -60,7 +62,7 @@ public class Waver : MonoBehaviour {
     /// </summary>
     public void AddFunc(float worldXEpicenter, int turns, float damp = 0.5f)
     {
-        _addWaves.Add(new Wave(MainAmplitude * 0.2f, Pindol200, DegreesPhase, Frequency * 10f, turns, damp, worldXEpicenter));
+        _addWaves.Add(new Wave(MainAmplitude * 0.2f, Pindol200, DegreesPhase, Frequency * 10f, StandingWaveCoeff, turns, damp, worldXEpicenter));
     }
 
     // Update is called once per frame
@@ -99,6 +101,7 @@ public class Wave
     private float Pindol200 = 45f;    //for 1 -> 1 sec is 1 degree
     private float DegreesPhase = 0f;
     private float Frequency = 10f; //1 in position is 10 in angle
+    private float StandingWaveCoeff = 10f;
     private bool IsMain = false;
 
     // Additional waves
@@ -109,23 +112,25 @@ public class Wave
 
     public bool IsActive { get { return TurnsActive - CurrentTurnActive >= 0; } }
 
-    public Wave(float amp, float pindol, float phase, float freq, int turnsActive = -1)
+    public Wave(float amp, float pindol, float phase, float freq, float stdWaveCoeff, int turnsActive = -1)
     {
         MainAmplitude = amp;
         Pindol200     = pindol;
         DegreesPhase  = phase;
         Frequency     = freq;
         TurnsActive   = turnsActive;
+        StandingWaveCoeff = stdWaveCoeff;
         IsMain = true;
     }
 
-    public Wave(float amp, float pindol, float phase, float freq, int turnsActive, float damp, float epicenter)
+    public Wave(float amp, float pindol, float phase, float freq, float stdWaveCoeff, int turnsActive, float damp, float epicenter)
     {
         MainAmplitude = amp;
         Pindol200 = pindol;
         DegreesPhase = phase;
         Frequency = freq;
         TurnsActive = turnsActive;
+        StandingWaveCoeff = stdWaveCoeff;
         DampingFactor = damp;
         WorldXEpicenter = epicenter;
         IsMain = false;
@@ -133,13 +138,16 @@ public class Wave
 
     public float GetY(float x, float currentTime)
     {
+        var omegaT = Mathf.Deg2Rad * (currentTime * Pindol200);
+        var kaIks = Mathf.Deg2Rad * ((x + DegreesPhase) * Frequency);
         if (IsMain)
         {
-            return Mathf.Sin(Mathf.Deg2Rad * ((x + DegreesPhase) * Frequency) + Mathf.Deg2Rad * (currentTime * Pindol200)) * MainAmplitude;
+            var standing = StandingWaveCoeff < Mathf.Epsilon ? 1f : Mathf.Cos(kaIks / StandingWaveCoeff + omegaT);
+            return Mathf.Sin(kaIks + omegaT) * standing * MainAmplitude;
         }
 
         float howFar = Mathf.Abs(WorldXEpicenter - x);
-        var y = Mathf.Pow(2.71828f, -howFar * DampingFactor) * Mathf.Cos(Mathf.Deg2Rad * ((x + DegreesPhase) * Frequency) + Mathf.Deg2Rad * (currentTime * Pindol200)) * MainAmplitude;
+        var y = Mathf.Pow(2.71828f, -howFar * DampingFactor) * Mathf.Cos(kaIks + omegaT) * MainAmplitude;
         if (TurnsActive != -1 && IsActive)
         {
             y *= (TurnsActive - CurrentTurnActive + 1) / (float)TurnsActive;
