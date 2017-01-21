963020ac4dc8dcf8ddc767b201619e2f7430d331
Siara Siarzewski <sephichi@gmail.com>
2017-01-21 11:15:52 +0100

    Fale dodatkowe

--- Assets/Scripts/Waver.cs
+++ Assets/Scripts/Waver.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Waver : MonoBehaviour {
 
@@ -9,6 +10,8 @@ public class Waver : MonoBehaviour {
     public float DegreesPhase = 0f;
     public float Frequency = 10f; //1 in position is 10 in angle
 
+    private Wave _mainWave;
+    private readonly List<Wave> _addWaves = new List<Wave>();
     private float _currentTime = 0f;
 
     public static Waver Instance
@@ -37,15 +40,18 @@ public class Waver : MonoBehaviour {
         DontDestroyOnLoad(gameObject);
     }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
+        _mainWave = new Wave(MainAmplitude, Pindol200, DegreesPhase, Frequency);
+        //_addWaves.Add(new Wave(MainAmplitude * 0.2f, Pindol200, DegreesPhase, Frequency * 10f, -1, 0.5f, 6.2f));
+        //_addWaves.Add(new Wave(MainAmplitude * 0.2f, Pindol200, DegreesPhase, Frequency * 10f, -1, 0.5f, 6.1f));
+        //_addWaves.Add(new Wave(MainAmplitude * 0.2f, Pindol200, DegreesPhase, Frequency * 10f, -1, 0.5f, -3f));
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 
     void FixedUpdate()
     {
@@ -54,6 +60,65 @@ public class Waver : MonoBehaviour {
 
     public float GetY(float x)
     {
-        return Mathf.Sin(Mathf.Deg2Rad * ((x + DegreesPhase)* Frequency) + Mathf.Deg2Rad * (_currentTime * Pindol200)) * MainAmplitude;
+        return 
+            _mainWave.GetY(x, _currentTime) +
+            _addWaves.Sum(wave => wave.GetY(x, _currentTime));
+    }
+}
+
+public class Wave
+{
+    // Main wave
+    private float MainAmplitude = 3f;
+    private float Pindol200 = 45f;    //for 1 -> 1 sec is 1 degree
+    private float DegreesPhase = 0f;
+    private float Frequency = 10f; //1 in position is 10 in angle
+    private bool IsMain = false;
+
+    // Additional waves
+    private float DampingFactor = 1f;
+    private int TurnsActive = 0;        //-1 special value - forever
+    private float WorldXEpicenter = 0f;
+    public int CurrentTurnActive = 0;
+
+    public Wave(float amp, float pindol, float phase, float freq, int turnsActive = -1)
+    {
+        MainAmplitude = amp;
+        Pindol200     = pindol;
+        DegreesPhase  = phase;
+        Frequency     = freq;
+        TurnsActive   = turnsActive;
+        IsMain = true;
+    }
+
+    public Wave(float amp, float pindol, float phase, float freq, int turnsActive, float damp, float epicenter)
+    {
+        MainAmplitude = amp;
+        Pindol200 = pindol;
+        DegreesPhase = phase;
+        Frequency = freq;
+        TurnsActive = turnsActive;
+        DampingFactor = damp;
+        WorldXEpicenter = epicenter;
+        IsMain = false;
+    }
+
+    public float GetY(float x, float currentTime)
+    {
+        if (IsMain)
+        {
+            return Mathf.Sin(Mathf.Deg2Rad * ((x + DegreesPhase) * Frequency) + Mathf.Deg2Rad * (currentTime * Pindol200)) * MainAmplitude;
+        }
+        if (TurnsActive == -1)
+        {
+            float howFar = Mathf.Abs(WorldXEpicenter - x);
+            if (howFar < 0.01) return 0f;
+            return Mathf.Pow(2.71828f, -howFar * DampingFactor) * Mathf.Cos(Mathf.Deg2Rad * ((x + DegreesPhase) * Frequency) + Mathf.Deg2Rad * (currentTime * Pindol200)) * MainAmplitude;
+        }
+        else if (CurrentTurnActive - TurnsActive > 0)
+        {
+            return Mathf.Sin(Mathf.Deg2Rad * ((x + DegreesPhase) * Frequency) + Mathf.Deg2Rad * (currentTime * Pindol200)) * MainAmplitude / (TurnsActive - CurrentTurnActive);
+        }
+        return 0f;
     }
 }
