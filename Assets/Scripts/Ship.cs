c44e3768969619da849c518f568c36028feb4e37
Wyjec <kamil.praca@gmail.com>
2017-01-21 13:25:31 +0100

    Ship dzialo

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -17,22 +17,36 @@ public class Ship : MonoBehaviour, Ihitable {
 
 
     Weapons _weapon;
+    private Vector3 myRotation = Vector3.zero;
     public static bool isDead;
     private bool _isWeaponChanging = false;
     private bool _isAiming = false;
+    private bool _isShake = false;
     public float power = 0;
+    public float amplutude = 0.1f;
+    public float speed = 1;
+    public float angle = 90;
     int _minPower, _maxPower;
     int death = 0;
     int kill = 0;
     int win = 0;
+    public float clampMin, clampMax;
+    private Vector3 _cameraPosition;
+
+    Transform gun;
 
 
     // Use this for initialization
     void Start () {
+        clampMin = -50;
+        clampMax = 50;
+        angle = 90;
+        gun = transform.FindChild("Dzialo");
         _minPower = 0;
         _maxPower = 100;
         _weapon = Weapons.Blast;
         isDead = false;
+
 
     }
 	
@@ -41,12 +55,16 @@ public class Ship : MonoBehaviour, Ihitable {
         if (isDead)
             return;
         ChangeWeapon();
-        PowerAdjust();   
+        Aim();
+        PowerAdjust();
+
+        transform.position = new Vector3(transform.position.x, Waver.Instance.GetY(transform.position.x), transform.position.z);
     }
 
 
     public void Die()
     {
+        isDead = true;
         Destroy(gameObject);
         //Some other cool stuff
     }
@@ -69,10 +87,20 @@ public class Ship : MonoBehaviour, Ihitable {
             power += 0.5f;
             power = Mathf.Clamp(power,(float)_minPower,(float)_maxPower);
         }
+        if (Input.GetKeyUp("space"))
+        {
+            _isShake = true;
+            power = 0;
+        }
+
     }
 
     void Aim()
     {
-        //Aiming
+        float z = -Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        angle += z;
+        angle = Mathf.Clamp(angle, clampMin, clampMax);
+        gun.eulerAngles = new Vector3(0, 0, angle);
     }
+
 }
