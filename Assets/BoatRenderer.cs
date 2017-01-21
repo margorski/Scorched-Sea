1e0355fb1dbfc248ac855ce25d431e1b6a6a1b8b
Wyjec <kamil.praca@gmail.com>
2017-01-21 11:38:17 +0100

    Basic Ship stuff

---
+++ Assets/BoatRenderer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+
+public class BoatRenderer : MonoBehaviour
+{
+
+    private int NumberOfSegments;
+    public List<Vector2> Points;
+
+    private int _noOfPoints { get { return NumberOfSegments + 1; } }
+    public LineRenderer LineToRenderTo;
+
+    // Use this for initialization
+    void Start()
+    {
+        LineToRenderTo = gameObject.GetComponent<LineRenderer>();
+
+        LineToRenderTo.numPositions = Points.Count + 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        CreatePoints();
+    }
+
+    public void CreatePoints()
+    {
+        float x;
+        float y;
+        float z = 0f;
+
+        int index = 0;
+        foreach (var item in Points)
+        {
+            x = item.x;
+            y = item.y + Waver.Instance.GetY(x);
+
+            LineToRenderTo.SetPosition(index++, new Vector3(x, y, z));
+        }
+        LineToRenderTo.SetPosition(index, new Vector3(Points[0].x, Points[0].y + Waver.Instance.GetY(Points[0].x), z));
+    }
+
+    public void Show()
+    {
+        LineToRenderTo.enabled = true;
+    }
+
+    public void Hide()
+    {
+        LineToRenderTo.enabled = false;
+    }
+}
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -2,15 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ship : MonoBehaviour {
+public interface Ihitable
+{
+    void Die();
+}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class Ship : MonoBehaviour, Ihitable {
+
+    public enum Weapons
+    {
+        Blast,
+        Storm
+    }
+
+
+    Weapons _weapon;
+    public static bool isDead;
+    private bool _isWeaponChanging = false;
+    private bool _isAiming = false;
+    public float power = 0;
+    int _minPower, _maxPower;
+    int death = 0;
+    int kill = 0;
+    int win = 0;
+
+
+    // Use this for initialization
+    void Start () {
+        _minPower = 0;
+        _maxPower = 100;
+        _weapon = Weapons.Blast;
+        isDead = false;
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        if (isDead)
+            return;
+        ChangeWeapon();
+        PowerAdjust();   
+    }
+
+
+    public void Die()
+    {
+        Destroy(gameObject);
+        //Some other cool stuff
+    }
+
+
+    void ChangeWeapon()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (_weapon == Weapons.Blast)
+                _weapon = Weapons.Storm;
+            else _weapon = Weapons.Blast;
+        }
+    }
+
+    void PowerAdjust()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            power += 0.5f;
+            power = Mathf.Clamp(power,(float)_minPower,(float)_maxPower);
+        }
+    }
+
+    void Aim()
+    {
+        //Aiming
+    }
 }
