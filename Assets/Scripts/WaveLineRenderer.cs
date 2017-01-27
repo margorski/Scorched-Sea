1feb161164de3b2234ca28cf51f338bc8029a21f
Wyjec <kamil.praca@gmail.com>
2017-01-27 14:46:35 +0100

    Playback of waves, slows them down

--- Assets/Scripts/WaveLineRenderer.cs
+++ Assets/Scripts/WaveLineRenderer.cs
@@ -5,12 +5,13 @@ using System.Linq;
 
 [RequireComponent(typeof(LineRenderer))]
 
-public class WaveLineRenderer : MonoBehaviour {
+public class WaveLineRenderer : MonoBehaviour, IRecordable {
 
     public float WorldXStart;
     public float WorldXEnd;
     public int NumberOfSegments;
     public bool IsCoreWave = false;
+    public bool _isRecording = false;
     public GameObject ChildPrefab;
     internal float Scale = 1f;
     internal float YOffset = 0f;
@@ -20,8 +21,11 @@ public class WaveLineRenderer : MonoBehaviour {
 
     public LineRenderer LineToRenderTo;
 
+    public List<Vector3> _singleFrameWavePosition;
+    public List<List<Vector3>> _allWavePositions;
     // Use this for initialization
     void Start () {
+        _allWavePositions = new List<List<Vector3>>();
         if (IsCoreWave && ChildPrefab != null)
         {
             var color = LineToRenderTo.startColor;
@@ -54,11 +58,13 @@ public class WaveLineRenderer : MonoBehaviour {
 
     void FixedUpdate()
     {
+      //  if(!Replays.playback)
         CreatePoints();
     }
 
     public void CreatePoints()
     {
+        _singleFrameWavePosition = new List<Vector3>();
         float x;
         float y;
         float z = 0f;
@@ -69,6 +75,7 @@ public class WaveLineRenderer : MonoBehaviour {
             y = Waver.Instance.GetY(x) * Scale + YOffset;
 
             LineToRenderTo.SetPosition(index, new Vector3(x, y, z));
+            _singleFrameWavePosition.Add(new Vector3(x, y, z));
         }
     }
 
@@ -81,4 +88,28 @@ public class WaveLineRenderer : MonoBehaviour {
     {
         LineToRenderTo.enabled = false;
     }
+
+    public IEnumerator Playback()
+    {
+
+        foreach (List<Vector3> outerList in _allWavePositions.ToList())
+        {
+            for (int i = 0; i < outerList.Count - 1; i++)
+            {
+                LineToRenderTo.SetPosition(i, outerList[i]);
+            }
+            yield return null;
+        }
+
+        // Would be nice to release this humongous amount of datas
+        _allWavePositions.Clear();
+        _singleFrameWavePosition.Clear();
+        Replays.playback = false;
+    }
+
+    public void Record()
+    {
+        _allWavePositions.Add(_singleFrameWavePosition);
+    }
+    
 }
